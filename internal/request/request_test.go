50c2bbc8e0564374928e3810090f520eac6e8694
lydan-rs <104039346+lydan-rs@users.noreply.github.com>
2025-09-08 19:31:41 +1000

    Changing Tests to be table driven. Adding additional bad requests.

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -8,23 +8,48 @@ import (
 )
 
 func TestRequestFromReader(t *testing.T) {
-	// Test: Good GET Request line
-	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
-	require.NoError(t, err)
-	require.NotNil(t, r)
-	assert.Equal(t, "GET", r.RequestLine.Method)
-	assert.Equal(t, "/", r.RequestLine.RequestTarget)
-	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
-	// Test: Good GET Request line with path
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
-	require.NoError(t, err)
-	require.NotNil(t, r)
-	assert.Equal(t, "GET", r.RequestLine.Method)
-	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
-	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+	goodRequests := []struct {
+		inRequest string
+		expectedParsed Request
+	}{
+		{
+			inRequest: "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n", 
+			expectedParsed: Request{RequestLine: RequestLine{Method: "GET", RequestTarget: "/", HttpVersion: "1.1"}},
+		},
+		{
+			inRequest: "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n", 
+			expectedParsed: Request{RequestLine: RequestLine{Method: "GET", RequestTarget: "/coffee", HttpVersion: "1.1"}},
+		},
+	}
 
-	// Test: Invalid number of parts in request line
-	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
-	require.Error(t, err)
+	badRequests := []string {
+		"/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		"get /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		"HELLO /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		"coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		" GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		"GET / http/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		"GET / HTTP1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		"GET / HTTP/1.1\rHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+	}
+
+
+	for i := range goodRequests {
+		request := &goodRequests[i].inRequest
+		expectedParsed := &goodRequests[i].expectedParsed
+
+		r, err := RequestFromReader(strings.NewReader(*request))
+		require.NoError(t, err)
+		require.NotNil(t, r)
+		assert.Equal(t, expectedParsed, r)
+		// assert.Equal(t, expectedParsed.RequestLine.Method, r.RequestLine.Method)
+		// assert.Equal(t, expectedParsed.RequestLine.RequestTarget, r.RequestLine.RequestTarget)
+		// assert.Equal(t, expectedParsed.RequestLine.HttpVersion, r.RequestLine.HttpVersion)
+	}
+
+	for i, test := range badRequests {
+		_, err := RequestFromReader(strings.NewReader(test))
+		require.Error(t, err, "Bad Test %v, Input:\n%v", i, test)
+	}
 }
